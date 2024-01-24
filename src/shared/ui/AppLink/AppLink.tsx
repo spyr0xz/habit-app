e25fd43c6e2ac50e79ed4f6d45bdb6eecd4b2411
Nikita <123288888+spyr0xz@users.noreply.github.com>
2024-01-24 09:13:12 +0300

    add navlink variety

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,22 +1,49 @@
 import { FC } from "react";
-import { LinkProps } from "react-router-dom";
+import { Link, LinkProps, NavLink } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./AppLink.module.scss";
-import { Link } from "react-router-dom";
+
+export enum AppLinkVariant {
+  PRIMARY = "primary",
+  NAV = "nav",
+}
 
 interface AppLinkProps extends LinkProps {
   className?: string;
+  activeClassName?: string;
+  variant?: AppLinkVariant;
 }
 
 export const AppLink: FC<AppLinkProps> = (props) => {
-  const { className, to, children, ...otherProps } = props;
+  const {
+    className,
+    to,
+    children,
+    variant = "primary",
+    activeClassName = "",
+    ...otherProps
+  } = props;
+
+  if (variant === AppLinkVariant.PRIMARY) {
+    return (
+      <Link
+        to={to}
+        className={classNames(cls.AppLink, {}, [className])}
+        {...otherProps}
+      >
+        {children}
+      </Link>
+    );
+  }
   return (
-    <Link
+    <NavLink
       to={to}
-      className={classNames(cls.AppLink, {}, [className])}
+      className={({ isActive }) =>
+        classNames(cls.AppLink, { [activeClassName]: isActive }, [className])
+      }
       {...otherProps}
     >
       {children}
-    </Link>
+    </NavLink>
   );
 };
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,35 +1,63 @@
-import { useTranslation } from 'react-i18next'
-import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './Sidebar.module.scss'
-import { AppLink } from 'shared/ui/AppLink/AppLink';
-import { routePath } from 'shared/config/routeConfig/routeConfig';
+import { useTranslation } from "react-i18next";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Sidebar.module.scss";
+import { AppLink, AppLinkVariant } from "shared/ui/AppLink/AppLink";
+import { AppRoutes, routePath } from "shared/config/routeConfig/routeConfig";
+import { useCallback, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 interface SidebarProps {
   className?: string;
 }
 
-export const Sidebar = ({className}: SidebarProps) => {
-  const {t, i18n} = useTranslation()
-  
+export const Sidebar = ({ className }: SidebarProps) => {
+  const { t, i18n } = useTranslation();
+  const [isActive, setIsActive] = useState(false);
+
+  const onClickHandler = () => {
+    setIsActive((prev) => !prev);
+  };
+
   return (
     <div className={classNames(cls.Sidebar, {}, [className])}>
       <div className={cls.logo}>
         <p>Logo</p>
       </div>
-      <div className={cls.items}>
-      <AppLink className={cls.item} to={routePath.main}>
-        <span>{t('Habits')}</span>
-      </AppLink>
-      <AppLink className={cls.item} to={routePath.todos}>
-        <span>{t('Todo List')}</span>
-      </AppLink>
-      <AppLink className={cls.item} to={routePath.analytics}>
-        <span>{t('Analytics')}</span>
-      </AppLink>
-      <AppLink className={cls.item} to={routePath.settings}>
-        <span>{t('Settings')}</span>
-      </AppLink>
+      <div className={classNames(cls.items, {}, [className])}>
+        <AppLink
+          variant={AppLinkVariant.NAV}
+          activeClassName={cls.active}
+          className={classNames(cls.item, {}, [className])}
+          to={routePath.main}
+          onClick={onClickHandler}
+        >
+          <span>{t("Habits")}</span>
+        </AppLink>
+        <AppLink
+          variant={AppLinkVariant.NAV}
+          activeClassName={cls.active}
+          className={cls.item}
+          to={routePath.todos}
+        >
+          <span>{t("Todo List")}</span>
+        </AppLink>
+        <AppLink
+          variant={AppLinkVariant.NAV}
+          activeClassName={cls.active}
+          className={cls.item}
+          to={routePath.analytics}
+        >
+          <span>{t("Analytics")}</span>
+        </AppLink>
+        <AppLink
+          variant={AppLinkVariant.NAV}
+          activeClassName={cls.active}
+          className={cls.item}
+          to={routePath.settings}
+        >
+          <span>{t("Settings")}</span>
+        </AppLink>
       </div>
     </div>
-  )
-}
+  );
+};
