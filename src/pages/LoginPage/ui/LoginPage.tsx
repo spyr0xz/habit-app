ee7df94f6537a15eade81f06e4c4f5699c4978ba
Nikita <123288888+spyr0xz@users.noreply.github.com>
2024-01-30 10:02:35 +0300

    login page adjustments

--- src/pages/LoginPage/ui/LoginPage.tsx
+++ src/pages/LoginPage/ui/LoginPage.tsx
@@ -1,20 +1,36 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './LoginPage.module.scss'
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./LoginPage.module.scss";
+import { Input } from "shared/ui/Input/Input";
+import { useTranslation } from "react-i18next";
 
 interface LoginPageProps {
   className?: string;
 }
 
+const LoginPage = ({ className }: LoginPageProps) => {
+  const { t } = useTranslation();
 
-const LoginPage = ({className}: LoginPageProps) => {
   return (
     <div className={classNames(cls.LoginPage, {}, [className])}>
-      <div className={cls.modal}>
-
-      MODAL MODAL
+      <h1 className={cls.title}>Habit App</h1>
+      <div className={cls.dialog}>
+        <div className={cls.content}>
+          <div className={cls.inputWrapper}>
+            <Input placeholder="Username" />
+            <Input placeholder="Password" />
+          </div>
+          <div className={cls.descr}>
+            <p>
+              {t('Don’t have an account?')} <span>Sign Up</span>
+            </p>
+            <p>
+              {t('Forgot password?')}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
